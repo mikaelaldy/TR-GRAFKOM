32f60915b1c24689ed81abed619d0a6a6913e0a8
gilangAS20 <84905621+gilangAS20@users.noreply.github.com>
2021-07-04 11:29:19 +0700

    Add files via upload
    garek copas dadike 1 file utama

---
+++ update jalan.cpp
@@ -0,0 +1,59 @@
+/////////////////////////////////////////jalan///////////////////////////////////////////////
+glBegin(GL_POLYGON); //jalan
+glColor3f(0.3, 0.3, 0.3);
+glVertex3f(-600, 0, 200); // kiri atas
+glVertex3f(-600, 0, 500); // kiri bawah 
+glVertex3f(600, 0, 500);
+glVertex3f(600, 0, 200);
+glEnd();
+
+glBegin(GL_POLYGON); //marka jalan
+glColor3f(1, 1, 1);
+glVertex3f(-550, 1, 340);
+glVertex3f(-550, 1, 360);
+glVertex3f(-400, 1, 360);
+glVertex3f(-400, 1, 340);
+glEnd();
+
+glBegin(GL_POLYGON); //marka jalan
+glColor3f(1, 1, 1);
+glVertex3f(-350, 1, 340);
+glVertex3f(-350, 1, 360);
+glVertex3f(-200, 1, 360);
+glVertex3f(-200, 1, 340);
+glEnd();
+
+glBegin(GL_POLYGON); //marka jalan
+glColor3f(1, 1, 1);
+glVertex3f(-150, 1, 340);
+glVertex3f(-150, 1, 360);
+glVertex3f(0, 1, 360);
+glVertex3f(0, 1, 340);
+glEnd();
+
+glBegin(GL_POLYGON); //marka jalan
+glColor3f(1, 1, 1);
+glVertex3f(50, 1, 340);
+glVertex3f(50, 1, 360);
+glVertex3f(200, 1, 360);
+glVertex3f(200, 1, 340);
+glEnd();
+
+glBegin(GL_POLYGON); //marka jalan
+glColor3f(1, 1, 1);
+glVertex3f(250, 1, 340);
+glVertex3f(250, 1, 360);
+glVertex3f(400, 1, 360);
+glVertex3f(400, 1, 340);
+glEnd();
+
+glBegin(GL_POLYGON); //marka jalan
+glColor3f(1, 1, 1);
+glVertex3f(450, 1, 340);
+glVertex3f(450, 1, 360);
+glVertex3f(600, 1, 360);
+glVertex3f(600, 1, 340);
+glEnd();
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
